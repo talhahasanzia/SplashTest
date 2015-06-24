af6b474c8633fdcddd59b035883a1820bd604e28
Talha <talhahasanzia@hotmail.com>
2015-06-24 17:11:52 +0500

    Created Slide
    Slide with slippery effect

---
+++ Splash/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rg.AddForce(-5,0,0);
+		
+		
+		}
+	
+	}
+}
